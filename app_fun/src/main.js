4f12f9b32e30a8d8eb975051ec2b4da784a97851
cj3636 <phillgilmartin@gmail.com>
2020-02-14 15:48:32 -0800

    Added Game Code.

---
+++ app_fun/src/main.js
@@ -0,0 +1,73 @@
+const BrowserWindow = require('electron').BrowserWindow;
+const app = require('electron').app;
+
+const path = require('path');
+const config = require('electron-json-config');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow = null;
+
+// Create the main Browser Window instance for the app
+const createWindow = () => {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 700,
+        minWidth: 800,
+        minHeight: 700,
+        frame: true,
+        backgroundColor: '#2e2c29',
+        //icon: path.join(__dirname, 'img/app.ico'),
+        webPreferences: {
+            contextIsolation: false,
+            nodeIntegration: true,
+            webviewTag: true,
+        },
+        show: false,
+    });
+
+    // Once the window is fully loaded, actually show it to the user
+    mainWindow.webContents.on('did-finish-load', () => {
+        mainWindow.show();
+    });
+
+    // and load the index.html of the app.
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+    // Some crappy workaround to make the window controls work
+    // without having to show the window before its ready
+    mainWindow.unmaximize();
+    if (config.get('devMode')) {
+        mainWindow.webContents.openDevTools();
+    }
+    // Emitted when the window is closed.
+    mainWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
